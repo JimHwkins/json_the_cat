c5ca2b3ccba9a031f721f4aa956cc9c5ad76d27d
JimHwkins <howlarrior@gmail.com>
2023-01-19 22:07:27 -0300

    Refactored

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,49 +1,26 @@
-const request = require('request');
-const readline = require('readline');
+const request = require("request");
 
-
-const requestBreedDescription = (breed) => {
-  request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, (error, response, body) => {
-    if (error) {
-      console.log('Something went wrong: ', error);
-      process.exit();
-    }
-  
-    const data = JSON.parse(body);
-  
-    const breed = data[0];
-    if (breed) {
-      console.log(breed.description);
-      process.exit();
-    } else {
-      console.log(`Failed to find breed ${breed}`);
-      process.exit();
-    }
-  });
-};
-
-const read = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-const breedFetcher = function() {
-  if (process.argv.length < 3) {
-    read.question("Please provide a cat breed as argument: ", (answer) => {
-      if (answer) {
-        requestBreedDescription(answer);
-      } else {
-        breedFetcher();
+const fetchBreedDescription = (breedName, cbFunction) => {
+  request(
+    'https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+      if (error) {
+        return cbFunction((error, null));
       }
-    });
-    
-  } else {
-    const nameBreed = process.argv[2];
-    requestBreedDescription(nameBreed);
-  }
+
+      const data = JSON.parse(body);
+
+      const breed = data[0];
+      if (breed) {
+        cbFunction(null, breed.description);
+      } else {
+        cbFunction(`Failed to find breed ${breedName}`);
+      }
+    }
+  );
 };
 
-breedFetcher();
+
+module.exports = { fetchBreedDescription };
 
 
 
